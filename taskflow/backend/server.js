cc2d0dade2d2427214dd40bce12d6374a3c28132
harishdtu <satyaharish12345@gmail.com>
2026-05-05 12:14:39 +0530

    changes

--- taskflow/backend/server.js
+++ taskflow/backend/server.js
@@ -17,6 +17,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: 'http://localhost:5173',
+  "https://task-management-jade-five.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
